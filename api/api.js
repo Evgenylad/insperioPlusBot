3ef74bce7dac55a04141b62160e14327268c736e
Evgeny Ladyzhenskiy <7384093@gmail.com>
2018-01-31 18:03:17 +0300

    trying to solve db issue

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const apiRouter = express.Router();
-const artistsRouter = require('./telegram.js');
+const telegramRouter = require('./telegram.js');
 
 apiRouter.use('/telegram', telegramRouter);
 
