7abe345da705e5e7f6601b0c6361482941ec9c6a
Evgeny Ladyzhenskiy <7384093@gmail.com>
2018-01-31 18:03:56 +0300

    trying to solve db issue

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const apiRouter = require('./api/api');
 //const sqlite3 = require('sqlite3');
 //const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
 
-const bot = require('./telegramBot');
-
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static('public'));
