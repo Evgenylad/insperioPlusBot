5c17b43a224bd6c8a5857fdef6df8c70da7d8047
Evgeny Ladyzhenskiy <7384093@gmail.com>
2018-01-31 18:19:12 +0300

    trying to solve db issue

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,6 @@
 const TELEGRAM_TOKEN = '516400841:AAF_JfiI53pYZejY2jW4lRguhJh1MTPepfY';
 const TELEGRAM_URL = 'https://api.telegram.org/bot';
-const API_URL = 'https://singleclick.ru/api';
+const API_URL = 'https://singleclick.ru/api/';
 
 module.exports = {
   TELEGRAM_TOKEN,
