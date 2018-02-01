72604ff47cafaddae54c926380988575c4430fe7
Evgeny Ladyzhenskiy <7384093@gmail.com>
2018-02-01 15:56:10 +0300

    https

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const apiRouter = require('./api/api');
 //const sqlite3 = require('sqlite3');
 //const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 4444;
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
@@ -36,7 +36,7 @@ http.createServer(function(req, res) {
 
 https.createServer(function(req, res) {
   console.log(req);
-}).listen(4444, () => {
+}).listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
