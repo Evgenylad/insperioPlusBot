78fead5fbdd086f0bc13e44547065790b03bba12
Evgeny Ladyzhenskiy <7384093@gmail.com>
2018-02-01 17:28:11 +0300

    https

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const apiRouter = require('./api/api');
 //const sqlite3 = require('sqlite3');
 //const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
 
-const PORT = process.env.PORT || 4444;
+const PORT = process.env.PORT || 443;
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
