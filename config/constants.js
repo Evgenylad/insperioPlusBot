d84f7480f31c849cf06ea30906c0e2295674e5d3
Evgeny Ladyzhenskiy <7384093@gmail.com>
2018-02-02 17:02:04 +0300

    creat new collection at db

--- config/constants.js
+++ config/constants.js
@@ -2,8 +2,8 @@ const TELEGRAM_TOKEN = '516400841:AAF_JfiI53pYZejY2jW4lRguhJh1MTPepfY';
 const TELEGRAM_URL = 'https://api.telegram.org/bot';
 const API_URL = 'https://singleclick.ru/api/';
 const ACEPTED_USERS = {
-  evgenyId: '100364646',
-  ksenyaId: '49916993'
+  evgenyId: 100364646,
+  ksenyaId: 49916993
 }
 
 module.exports = {
