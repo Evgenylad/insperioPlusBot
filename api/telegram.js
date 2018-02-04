f463c71e3cc5889d75a7b4ab7c58c4dc373b6b74
Evgeny Ladyzhenskiy <7384093@gmail.com>
2018-02-04 10:56:37 +0300

    create current payment obj"

--- api/telegram.js
+++ api/telegram.js
@@ -89,16 +89,17 @@ api.on('message', function(message)
             if (err) throw err;
             let myQuery = {user: user, lastMessage: lastUserMessage};
             db.collection('messages').find({}).toArray(function(err, result) {
-              console.log(result);
 
               if (err) throw err;
               if (!result) {
+                console.log('result1', result);
                 db.collection('messages').insertOne(myQuery, function(err, result) {
                   if (err) throw err;
                   console.log('result of wrighting', result);
                   client.close();
                 });
               } else if (user === result){
+                console.log('result2', result);
                 db.collection('messages').drop();
                 db.collection('messages').insertOne(myQuery, function(err, result) {
                   if (err) throw err;
